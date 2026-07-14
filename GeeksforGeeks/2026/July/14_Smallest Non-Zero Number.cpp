5c4479788b670d08a96f62ee954e1adc7790ef52
SASI KIRAN PALURU <palurusasikiran@gmail.com>
2026-07-14 22:42:41 +0530

    Create 14_Smallest Non-Zero Number.cpp

---
+++ GeeksforGeeks/2026/July/14_Smallest Non-Zero Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int find(vector<int>& arr) {
+        long long need = 0;
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            need = (need + arr[i] + 1) / 2;   // ceil((need + arr[i]) / 2)
+        }
+
+        return max(1LL, need);
+    }
+};
